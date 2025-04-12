2467e9fa0efa6d9b39fa6617badb94b4cecbff0b
Christian Hammermeister <christian.hammermeister@borderless-technologies.com>
2025-04-12 12:18:29 +0200

    create adapter for axum

--- executor/runner/src/server.rs
+++ executor/runner/src/server.rs
@@ -3,7 +3,6 @@ use axum::{
     body::{to_bytes, Body},
     extract::State,
     http::{Request, Response},
-    routing::any,
     Router,
 };
 use borderless_kv_store::Db;
@@ -16,6 +15,7 @@ async fn contract_handler(
     req: Request<Body>,
 ) -> Response<Body> {
     let (parts, body) = req.into_parts();
+
     // 10MB upper limit
     let bytes = to_bytes(body, 10_000_000).await.unwrap_or_default();
 
@@ -28,9 +28,9 @@ pub async fn start_contract_server(db: impl Db + 'static) -> Result<()> {
     let srv = RtService::new(db)?;
 
     // Create a router and attach the custom service to a route
-    let app = Router::new()
-        .route("/v0/contract/{*path}", any(contract_handler))
-        .with_state(srv);
+    let contract = Router::new().fallback(contract_handler).with_state(srv);
+
+    let app = Router::new().nest("/v0/contract", contract);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
     info!("Listening on {}", listener.local_addr()?);
