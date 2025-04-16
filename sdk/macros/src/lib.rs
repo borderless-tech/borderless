b6616355456569ec49a41b781b29526c3a5877f7
Christian Hammermeister <christian.hammermeister@borderless-technologies.com>
2025-04-16 15:40:41 +0200

    make macro generate wasm exports

--- sdk/macros/src/lib.rs
+++ sdk/macros/src/lib.rs
@@ -1,17 +1,65 @@
 use proc_macro::TokenStream;
-use syn::parse_macro_input;
+use quote::quote;
+use syn::{parse_macro_input, Item, ItemMod};
+
+mod contract;
 
 #[proc_macro_attribute]
 pub fn contract(_attrs: TokenStream, input: TokenStream) -> TokenStream {
-    // let input = parse_macro_input!(input);
-    // let output = state::impl_contract_state(input);
+    let module = parse_macro_input!(input as ItemMod);
 
-    // match output {
-    //     syn::Result::Ok(token_stream) => token_stream,
-    //     syn::Result::Err(err) => err.to_compile_error(),
-    // }
-    // .into()
-    input
+    // Check if module has some content
+    if module.content.is_none() {
+        return syn::Error::new_spanned(
+            module,
+            "Macro can only be implemented on modules that have some content",
+        )
+        .to_compile_error()
+        .into();
+    }
+    let (brace, mut items) = module.content.unwrap();
+
+    // Generate new tokens based on the module's content
+    let new_tokens = match contract::parse_module_content(brace.span.join(), &items, &module.ident)
+    {
+        Ok(tokens) => tokens,
+        Err(e) => e.to_compile_error(),
+    };
+
+    // Add these new tokens to the existing items
+    items.push(Item::Verbatim(new_tokens));
+
+    // Also generate an error if the visibility is not public
+    match module.vis {
+        syn::Visibility::Public(_) => (),
+        _ => {
+            let tokens =
+                syn::Error::new_spanned(&module.mod_token, "Contract module must be public")
+                    .to_compile_error()
+                    .into();
+            items.push(Item::Verbatim(tokens));
+        }
+    }
+
+    let wasm_exports = contract::generate_wasm_exports(&module.ident);
+
+    // Generate a new module from the content of the original module
+    let new_module = ItemMod {
+        attrs: module.attrs,
+        vis: module.vis,
+        unsafety: module.unsafety,
+        mod_token: module.mod_token,
+        ident: module.ident,
+        content: Some((brace, items)),
+        semi: module.semi,
+    };
+
+    // Convert into token stream
+    quote! {
+        #new_module
+        #wasm_exports
+    }
+    .into()
 }
 
 #[proc_macro_attribute]
