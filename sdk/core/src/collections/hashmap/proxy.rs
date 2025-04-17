4aeb910da894392c8c89667883582b438fc885a0
Lluís Oriol Colom Nichols <lluis.colom@borderless-technologies.com>
2025-04-17 12:32:07 +0200

    Update Proxy and introduce ProxyMut

--- sdk/core/src/collections/hashmap/proxy.rs
+++ sdk/core/src/collections/hashmap/proxy.rs
@@ -1,20 +1,20 @@
-use std::borrow::Borrow;
+use std::borrow::{Borrow, BorrowMut};
 use std::cell::RefCell;
 use std::marker::PhantomData;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 use std::rc::Rc;
 
 use super::cache::Cell;
 
 pub struct Proxy<'a, V> {
-    pub(super) value_ptr: RefCell<Rc<Cell<V>>>,
+    pub(super) cell_ptr: Rc<RefCell<Cell<V>>>,
     pub(super) _back_ref: PhantomData<&'a V>, // <- prevents the tree from being borrowed mutably, while a proxy object exists
 }
 
 impl<'a, V> AsRef<V> for Proxy<'a, V> {
     fn as_ref(&self) -> &V {
         // TODO - check if this causes UB !
-        let p = unsafe { &mut *self.value_ptr.as_ptr() };
+        let p = unsafe { &mut *self.cell_ptr.as_ptr() };
         &p.value
     }
 }
@@ -30,5 +30,52 @@ impl<'a, V> Deref for Proxy<'a, V> {
 impl<'a, V> Borrow<V> for Proxy<'a, V> {
     fn borrow(&self) -> &V {
         self.as_ref()
+    }
+}
+
+pub struct ProxyMut<'a, V> {
+    pub(super) cell_ptr: Rc<RefCell<Cell<V>>>,
+    pub(super) _back_ref: PhantomData<&'a mut V>, // <- prevents the tree from being borrowed, while a proxy object exists
+}
+
+impl<'a, V> AsRef<V> for ProxyMut<'a, V> {
+    fn as_ref(&self) -> &V {
+        // TODO - check if this causes UB !
+        let p = unsafe { &mut *self.cell_ptr.as_ptr() };
+        &p.value
+    }
+}
+
+impl<'a, V> AsMut<V> for ProxyMut<'a, V> {
+    fn as_mut(&mut self) -> &mut V {
+        // TODO - check if this causes UB !
+        let p = unsafe { &mut *self.cell_ptr.as_ptr() };
+        &mut p.value
+    }
+}
+
+impl<'a, V> Deref for ProxyMut<'a, V> {
+    type Target = V;
+
+    fn deref(&self) -> &Self::Target {
+        self.as_ref()
+    }
+}
+
+impl<'a, V> DerefMut for ProxyMut<'a, V> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.as_mut()
+    }
+}
+
+impl<'a, V> Borrow<V> for ProxyMut<'a, V> {
+    fn borrow(&self) -> &V {
+        self.as_ref()
+    }
+}
+
+impl<'a, V> BorrowMut<V> for ProxyMut<'a, V> {
+    fn borrow_mut(&mut self) -> &mut V {
+        self.as_mut()
     }
 }
