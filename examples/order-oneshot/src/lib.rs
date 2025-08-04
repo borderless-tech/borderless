2b7e48f4e6468101c493f4fd921a7e3de90d273a
Christian Hammermeister <christian.hammermeister@borderless-technologies.com>
2025-08-04 19:21:27 +0200

    test ledger api

--- examples/order-oneshot/src/lib.rs
+++ examples/order-oneshot/src/lib.rs
@@ -1,12 +1,10 @@
 #[borderless::contract]
 pub mod order_oneshot {
-    use borderless::{prelude::*, serialize::Value};
+    use borderless::prelude::*;
 
     #[derive(State)]
     pub struct Order {
-        items: Value,
-        /// 'Due-Date'
-        ts_due: u64,
+        cnt: u64,
     }
     /*
      * Order Requested
@@ -14,5 +12,35 @@ pub mod order_oneshot {
      * Order
      * */
 
-    impl Order {}
+    impl Order {
+        #[action]
+        pub fn buy(&mut self) -> Result<()> {
+            ledger::transfer("buyer", "seller")
+                .with_amount("85 €".parse()?)
+                .with_tax("16,15 €".parse()?)
+                .with_tag(format!("bought item-{}", self.cnt))
+                .execute()?;
+            self.cnt += 1;
+            Ok(())
+        }
+
+        #[action]
+        pub fn settle(&self, item_no: u64) -> Result<()> {
+            ledger::settle_debt("buyer", "seller")
+                .with_amount("85 €".parse()?)
+                .with_tag(format!("settle item-{}", item_no))
+                .execute()?;
+            Ok(())
+        }
+
+        #[action]
+        pub fn cancel_last(&mut self) -> Result<()> {
+            ledger::settle_debt("buyer", "seller")
+                .with_amount("85 €".parse()?)
+                .with_tag(format!("cancel item-{}", self.cnt.saturating_sub(1)))
+                .execute()?;
+            self.cnt -= 1;
+            Ok(())
+        }
+    }
 }
