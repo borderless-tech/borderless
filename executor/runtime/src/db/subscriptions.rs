bb9f7ee54b93a1244a9e675a040139a2a5c0c2da
Lluís Oriol Colom Nichols <lluis.colom@borderless-technologies.com>
2025-08-11 10:12:15 +0200

    Remove kickoff comment explaining how to handle subscriptions

--- executor/runtime/src/db/subscriptions.rs
+++ executor/runtime/src/db/subscriptions.rs
@@ -357,23 +357,3 @@ mod tests {
         Ok(())
     }
 }
-
-/*
- * subscribe(cid, "/order/change", "my_action");
- *
- * Message { topic, value } <<<< Bake in the contract-id or agent-id of the emitter of the message
- * -> lookup subscribers -> agent-id
- * -> "my_action" -> CallAction { method_name: "my_action", value }
- * -> call action with agent-id and action-struct
- *
- * fn convert_messages(msgs: Vec<Message>, db: &'a DB) -> Vec<(AgentId, CallAction)> { ... }
- *
- *
- *
- * A message from contract cc963345-4cd9-8f30-b215-2cdffee3d189 on topic /foo/baaa should become:
- *
- * /cc963345-4cd9-8f30-b215-2cdffee3d189/foo/baa
- * (this gives you the option of splitting out the ID)
- *
- * -> do a lowercase conversion, so e.g. MY-TOPIC and My-topic would become my-topic
- */
